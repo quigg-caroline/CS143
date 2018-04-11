3b9e42839b2e16140f883b9cfd19668e91273940
Caroline-Quigg <carolinequigg@yahoo.com>
2018-04-11 08:00:14 -0700

    calculator

--- sample.php
+++ sample.php
@@ -1,12 +1,40 @@
 <!DOCTYPE html>
 <html>
+<head>
+
+</head>
+
 <body>
 
-<h1>My first PHP page</h1>
+<h1>PHP Calculator</h1>
+Type an expression in the box </br>
+
+<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+<input type="text" name="expression">
+<input type="submit" value="Calculate">
+</form>
 
 <?php
-echo "Hello World!";
+ if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 	$expression = $_POST['expression'];
+ 	if (empty($expression)){
+ 		echo "Empty expression";
+ 	} else {
+ 		$pattern2 = "/^([-+]? ?(\d+|\(\g<1>\))( ?[-+*\/] ?\g<1>)?)$/";
+ 		$pattern = "/^((?![0-9]).)+/";
+ 		if (preg_match($pattern2,$expression,$match)){
+ 			$result =eval("return ".$expression.";");
+ 			echo $result;
+
+ 		}
+ 		else{
+ 			echo "Bad expression";
+ 		}
+
+ 	}
+ }
 ?>
 
 </body>
+
 </html>
