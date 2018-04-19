4ee6cafc8ce1fcf1980be26a09522e0a586aecf9
NatashaKohli <natashakohli@ucla.edu>
2018-04-18 21:11:25 -0700

    updated 3rd query

--- 1B/queries.sql
+++ 1B/queries.sql
@@ -10,22 +10,9 @@ FROM (
   HAVING count > 1
 ) A1;
 
-
-
-
-SELECT mid, COUNT(*) as count FROM MovieGenre GROUP BY mid
-HAVING genre = "Romance" AND genre = "Comedy";
-
--- WHERE A.mid = B.mid AND A.genre <> B.genre;
-
-SELECT Director.first, Director.last, MovieDirector.mid, GROUP_CONCAT(MovieGenre.genre) as genres
+SELECT Director.first, Director.last as genres
 FROM Director
 INNER JOIN MovieDirector ON Director.id = MovieDirector.did
 INNER JOIN MovieGenre ON MovieDirector.mid = MovieGenre.mid
-WHERE MovieGenre.genre = "Romance" OR MovieGenre.genre = "Comedy"
+WHERE MovieGenre.genre = "Romance"
 GROUP BY MovieGenre.mid;
--- GROUP BY MovieGenre.mid;
-
-SELECT genre
-FROM MovieGenre
-WHERE mid = 4536;
