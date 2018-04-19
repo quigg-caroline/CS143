3e8973757f93d4f3411241fa8f7ac1239d024ecf
NatashaKohli <natashakohli@ucla.edu>
2018-04-18 21:41:28 -0700

    added constraints

--- 1B/create.sql
+++ 1B/create.sql
@@ -5,6 +5,7 @@ CREATE TABLE Movie (
   rating VARCHAR(100),
   company VARCHAR(50),
   PRIMARY KEY (id)
+	-- Movie id should be unique
 );
 
 CREATE TABLE Actor(
@@ -14,7 +15,10 @@ CREATE TABLE Actor(
 	sex VARCHAR(6),
 	dob DATE,
 	dod DATE,
-	PRIMARY KEY (id)
+	PRIMARY KEY (id),
+	-- Actor id should be unique
+	CHECK (dod IS NULL OR YEAR(dod) > YEAR(dob))
+	-- Actor's death year should be after his/her birth year (or the actor isn't dead yet)
 );
 
 
@@ -24,27 +28,42 @@ CREATE TABLE Director(
 	first VARCHAR(20),
 	dob DATE,
 	dod DATE,
-	PRIMARY KEY (id)
+	PRIMARY KEY (id),
+	-- Director id should be unique
+	CHECK (dob IS NOT NULL)
+	-- Director must have been born
 );
 
 CREATE TABLE MovieGenre(
 	mid INT,
 	genre VARCHAR(20),
-	PRIMARY KEY (mid, genre)
-);
+	PRIMARY KEY (mid, genre),
+	-- Every row should be unique (movie genre pairing)
+	FOREIGN KEY (mid) REFERENCES Movie(id)
+	-- More information on the movie through the movie id
+) ;
 
 CREATE TABLE MovieDirector(
 	mid INT,
 	did INT,
-	PRIMARY KEY(mid)
-);
+	PRIMARY KEY(mid),
+	FOREIGN KEY (mid) REFERENCES Movie(id),
+	-- More information on the movie through the movie id
+	FOREIGN KEY (did) REFERENCES Director(id)
+	-- More information on the director through the director id
+) ;
 
 CREATE TABLE MovieActor(
 	mid INT,
 	aid INT,
 	role VARCHAR(50),
-	PRIMARY KEY (mid,aid)
-);
+	PRIMARY KEY (mid,aid),
+	-- Every row should be unique (movie actor pairing)
+	FOREIGN KEY (mid) REFERENCES Movie(id),
+	-- More information on the movie through the movie id
+	FOREIGN KEY (aid) REFERENCES Actor(id)
+	-- More information on the actor through the actor id
+) ;
 
 CREATE TABLE Review(
 	name VARCHAR(20),
@@ -52,8 +71,11 @@ CREATE TABLE Review(
 	mid INT,
 	rating INT,
 	comment VARCHAR(500),
-	PRIMARY KEY (name, mid)
-);
+	FOREIGN KEY (mid) REFERENCES Movie(id),
+	-- More information on the movie through the movie id
+	CHECK (rating >= 0)
+	-- Rating cannot be negative
+) ;
 
 CREATE TABLE MaxPersonID(
 	id INT
