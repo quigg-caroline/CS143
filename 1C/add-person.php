27f5a55726ec9472510c2cdc4789d39d8271d142
NatashaKohli <natashakohli@ucla.edu>
2018-04-22 17:13:06 -0700

    simple html for add person page

---
+++ 1C/add-person.php
@@ -0,0 +1,34 @@
+<html>
+
+<body>
+
+<h2>Add a new actor or director!</h2>
+
+<form>
+  <div>
+    <input type = "radio" name = "job" /> Actor  
+    <input type = "radio" name = "job" /> Director
+  </div>
+
+  <div>
+    First Name <input type = "text" placeholder = "First Name" />
+    Last Name <input type = "text" placeholder = "Last Name" />
+  </div>
+
+  <div>
+    <input type = "radio" name = "gender" /> Male  
+    <input type = "radio" name = "gender" /> Female
+  </div>
+
+  <div>
+    Date of Birth <input type = "text" placeholder = "Date of Birth" />
+    Date of Death <input type = "text" placeholder = "Date of Death" />
+  </div>
+
+  <button type = "submit" >Add</button>
+
+</form>
+
+</body>
+
+</html>
