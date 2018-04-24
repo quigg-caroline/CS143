4e4d153483921bea4649419f1dcfeb91d503ea72
NatashaKohli <natashakohli@ucla.edu>
2018-04-24 16:18:44 -0700

    simple html for movie page and homepage

---
+++ 1C/add-movie.php
@@ -0,0 +1,42 @@
+<html>
+
+<body>
+
+<h2>Add a new movie!</h2>
+
+<form>
+
+  <div>
+    Title <input type = "text" placeholder = "Title" />
+    Company <input type = "text" placeholder = "Company" />
+    Year <input type = "text" placeholder = "Year" />
+    MPAA Rating 
+    <select>
+      <option value = "G">G</option>
+      <option value = "NC-17">NC-17</option>
+      <option value = "PG">PG</option>
+      <option value = "PG-13">PG-13</option>
+      <option value = "R">R</option>
+    </select>
+  </div>
+
+  <div>
+    <input type = "checkbox" /> Action  
+    <input type = "checkbox" /> Adult
+    <input type = "checkbox" /> Adventure  
+    <input type = "checkbox" /> Animation
+    <input type = "checkbox" /> Comedy  
+    <input type = "checkbox" /> Crime
+    <input type = "checkbox" /> Documentary 
+    <input type = "checkbox" /> Drama
+    <input type = "checkbox" /> Family  
+    <input type = "checkbox" /> Fantasy
+  </div>
+
+  <button type = "submit" >Add</button>
+
+</form>
+
+</body>
+
+</html>
---
+++ 1C/homepage.php
@@ -0,0 +1,2 @@
+<a href = "add-person.php">Add Actor/Director</a>
+<a href = "add-movie.php">Add Movie</a>
