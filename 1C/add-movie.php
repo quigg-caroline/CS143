de99df7347e80f6ac950fa29eefab65ebb2a474e
NatashaKohli <natashakohli@ucla.edu>
2018-04-25 16:46:45 -0700

    save values from form in php

--- 1C/add-movie.php
+++ 1C/add-movie.php
@@ -4,14 +4,14 @@
 
 <h2>Add a new movie!</h2>
 
-<form>
+<form method = "post">
 
   <div>
-    Title <input type = "text" placeholder = "Title" />
-    Company <input type = "text" placeholder = "Company" />
-    Year <input type = "text" placeholder = "Year" />
+    Title <input type = "text" name = "title" placeholder = "Title" />
+    Company <input type = "text" name = "company" placeholder = "Company" />
+    Year <input type = "text" name = "year" placeholder = "Year" />
     MPAA Rating 
-    <select>
+    <select name = "rating">
       <option value = "G">G</option>
       <option value = "NC-17">NC-17</option>
       <option value = "PG">PG</option>
@@ -21,21 +21,42 @@
   </div>
 
   <div>
-    <input type = "checkbox" /> Action  
-    <input type = "checkbox" /> Adult
-    <input type = "checkbox" /> Adventure  
-    <input type = "checkbox" /> Animation
-    <input type = "checkbox" /> Comedy  
-    <input type = "checkbox" /> Crime
-    <input type = "checkbox" /> Documentary 
-    <input type = "checkbox" /> Drama
-    <input type = "checkbox" /> Family  
-    <input type = "checkbox" /> Fantasy
+    Genre:
+    <input type = "checkbox" name = "genre[]" value = "Action" /> Action  
+    <input type = "checkbox" name = "genre[]" value = "Adult" /> Adult
+    <input type = "checkbox" name = "genre[]" value = "Adventure" /> Adventure  
+    <input type = "checkbox" name = "genre[]" value = "Animation" /> Animation
+    <input type = "checkbox" name = "genre[]" value = "Comedy" /> Comedy  
+    <input type = "checkbox" name = "genre[]" value = "Crime" /> Crime
+    <input type = "checkbox" name = "genre[]" value = "Documentary" /> Documentary 
+    <input type = "checkbox" name = "genre[]" value = "Drama" /> Drama
+    <input type = "checkbox" name = "genre[]" value = "Family" /> Family  
+    <input type = "checkbox" name = "genre[]" value = "Fantasy" /> Fantasy
   </div>
 
-  <button type = "submit" >Add</button>
+  <button type = "submit" name = "submit" >Add</button>
 
 </form>
+
+<?php
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $title = $_POST['title'];
+    $company = $_POST['company'];
+    $year = $_POST['year'];
+    $rating = $_POST['rating'];
+    if (!empty($_POST['genre'])) {
+      foreach($_POST['genre'] as $selected)
+        echo $selected. '</br>';
+    }
+
+    $db = new mysqli('localhost', 'cs143', '', 'TEST');
+
+    if ($db->connect_errno > 0){
+      die('Unable to connect to database');
+    }
+  }
+?>
 
 </body>
 
