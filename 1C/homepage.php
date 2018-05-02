952f7dbe54f98504514958a9ea156959ade3d862
NatashaKohli <natashakohli@ucla.edu>
2018-05-01 22:32:01 -0700

    homepage blurb update

--- 1C/homepage.php
+++ 1C/homepage.php
@@ -5,6 +5,7 @@
 <div class = "home" >
   <div class = "header" >Welcome to... OUR WEBSITE!</div>
   <img class = "us" src = "the_developers.jpg" />
+  <div>(those are the developers with a cute little corgi)</div>
 </div>
 
 
