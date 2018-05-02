ff223e598a9ce466a03ed3eb7817dc242f139705
Caroline-Quigg <carolinequigg@yahoo.com>
2018-05-01 22:51:39 -0700

    relation updates

--- 1C/add-actorrelation.php
+++ 1C/add-actorrelation.php
@@ -5,13 +5,46 @@
 <h2>Add an actor to a movie!!</h2>
 
 <form method = "post">
-
   <div>
-    Actor <input type = "text" name = "name" placeholder = "Full Name" />
+    Movie
+    <select name="movie">
+      <?php
+         $db = new mysqli('localhost', 'cs143', '', 'TEST');
+
+        if ($db->connect_errno > 0){
+          die('Unable to connect to database');
+        }
+
+        $query_movieid = "SELECT title, year,id FROM Movie ORDER BY title ASC";
+        $result = $db->query($query_movieid);
+        while($row=$result->fetch_assoc())
+        {
+          echo "<option value=".$row['id'].">".$row['title']."(".$row['year'].")"."</option>";
+        }
+
+      ?>
+      </select>
   </div>
 
-  <div>
-    Movie <input type = "text" name = "movie" placeholder = "Movie Name" />
+    <div>
+    Actor
+    <select name="actor">
+      <?php
+         $db = new mysqli('localhost', 'cs143', '', 'TEST');
+
+        if ($db->connect_errno > 0){
+          die('Unable to connect to database');
+        }
+
+        $query_movieid = "SELECT CONCAT(first,\" \",last) as name,dob, id FROM Actor ORDER BY name ASC";
+        $result = $db->query($query_movieid);
+        while($row=$result->fetch_assoc())
+        {
+          echo "<option value=".$row['id'].">".$row['name']."(".$row['dob'].")"."</option>";
+        }
+
+      ?>
+      </select>
   </div>
 
   <div>
@@ -26,51 +59,36 @@
   if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (isset($_POST['submit'])) {
 
-      //TODO: add formatting checks
-      $name = $_POST['name'];
-      $movie_name = $_POST['movie'];
-      $role = $_POST['role'];
-      $actor_names = explode(" ", $name);
-      if (count($actor_names) < 2) {
-        exit("Enter first and last name for actor");
-      }
 
-      //query movie table for movie id
-       //Connect to DB
-      $db = new mysqli('localhost', 'cs143', '', 'TEST');
+      $get_string =  $_SERVER['QUERY_STRING'];
+      if (empty ($get_string))
+      {
 
-      if ($db->connect_errno > 0){
-        die('Unable to connect to database');
-      }
+        //TODO: add formatting checks
+        $actor = $_POST['actor'];
+        if(empty($actor))exit("Enter actor name.");
+        $movie = $_POST['movie'];
+        if(empty($movie))exit("Enter movie title.");
+        $role = $_POST['role'];
+        if(empty($role))exit("Enter actor role.");
 
-      $query_movieid = "SELECT id FROM Movie WHERE title = \"".$movie_name."\"";
-      $result = $db->query($query_movieid);
-      $movie = null;
-      if ($result->num_rows > 0) {
-        $row = $result->fetch_assoc();
-        $movie = $row['id'];
+
+        //query movie table for movie id
+         //Connect to DB
+        $db = new mysqli('localhost', 'cs143', '', 'TEST');
+
+        if ($db->connect_errno > 0){
+          die('Unable to connect to database');
+        }
+
+        
+        //insert into movie actor
+        $query_movieactor = $db->prepare("INSERT INTO MovieActor (mid, aid, role) VALUES (?, ?, ?)");
+        $query_movieactor->bind_param("iis", $movie, $actor, $role);
+          if ($query_movieactor->execute() === FALSE) {
+            echo "Error: " . $query_actor . "<br>" . $db->error;
+          }
       }
-      else {
-        exit("Error: Could not find movie");
-      }
-        $actor = null;
-        //query for actor id
-        $query_actorid = "SELECT id FROM Actor WHERE first = \"".$actor_names[0]."\" AND last=\"".$actor_names[1]."\"";
-        $result = $db->query($query_actorid);
-        if ($result->num_rows > 0) {
-          $row = $result->fetch_assoc();
-            $actor = $row['id'];
-          
-        }
-        else {
-          exit("Could not find actor");
-        }
-      //insert into movie actor
-      $query_movieactor = $db->prepare("INSERT INTO MovieActor (mid, aid, role) VALUES (?, ?, ?)");
-      $query_movieactor->bind_param("iis", $movie, $actor, $role);
-        if ($query_movieactor->execute() === FALSE) {
-          echo "Error: " . $query_actor . "<br>" . $db->error;
-        }
 
     }
   }
--- 1C/add-moviedirector.php
+++ 1C/add-moviedirector.php
@@ -6,12 +6,46 @@
 
 <form method = "post">
 
-  <div>
-    Movie <input type = "text" name = "movie" placeholder = "Movie Name" />
+ <div>
+    Movie
+    <select name="movie">
+      <?php
+         $db = new mysqli('localhost', 'cs143', '', 'TEST');
+
+        if ($db->connect_errno > 0){
+          die('Unable to connect to database');
+        }
+
+        $query_movieid = "SELECT title, year,id FROM Movie ORDER BY title ASC";
+        $result = $db->query($query_movieid);
+        while($row=$result->fetch_assoc())
+        {
+          echo "<option value=".$row['id'].">".$row['title']."(".$row['year'].")"."</option>";
+        }
+
+      ?>
+      </select>
   </div>
 
   <div>
-    Director <input type = "text" name = "name" placeholder = "Full Name" />
+    Director
+    <select name="director">
+      <?php
+         $db = new mysqli('localhost', 'cs143', '', 'TEST');
+
+        if ($db->connect_errno > 0){
+          die('Unable to connect to database');
+        }
+
+        $query_movieid = "SELECT CONCAT(first,\" \",last) as name,dob, id FROM Director ORDER BY name ASC";
+        $result = $db->query($query_movieid);
+        while($row=$result->fetch_assoc())
+        {
+          echo "<option value=".$row['id'].">".$row['name']."(".$row['dob'].")"."</option>";
+        }
+
+      ?>
+      </select>
   </div>
 
   <button type = "submit" name = "submit" >Add Director</button>
@@ -23,12 +57,11 @@
     if (isset($_POST['submit'])) {
 
       //TODO: add formatting checks
-      $movie_name = $_POST['movie'];
-      $director = $_POST['name'];
-      $director_names = explode(" ", $director);
-      if (count($director_names) < 2) {
-        exit("Enter first and last name for actor");
-      }
+      $movie= $_POST['movie'];
+              if(empty($movie))exit("Enter movie name.");
+      $did = $_POST['director'];
+              if(empty($did))exit("Enter director name.");
+    
 
       //query movie table for movie id
        //Connect to DB
@@ -38,28 +71,6 @@
         die('Unable to connect to database');
       }
 
-      $query_movieid = "SELECT id FROM Movie WHERE title = \"".$movie_name."\"";
-      $result = $db->query($query_movieid);
-      $movie = null;
-      if ($result->num_rows > 0) {
-        $row = $result->fetch_assoc();
-        $movie = $row['id'];
-      }
-      else {
-        exit("Error: Could not find movie");
-      }
-        $did = null;
-        //query for actor id
-        $query_directorid = "SELECT id FROM Director WHERE first = \"".$director_names[0]."\" AND last=\"".$director_names[1]."\"";
-        $result = $db->query($query_directorid);
-        if ($result->num_rows > 0) {
-          $row = $result->fetch_assoc();
-            $did = $row['id'];
-          
-        }
-        else {
-          exit("Could not find director");
-        }
       //insert into movie actor
       $query_moviedirector = $db->prepare("INSERT INTO MovieDirector (mid, did) VALUES (?, ?)");
       $query_moviedirector->bind_param("ii", $movie, $did);
