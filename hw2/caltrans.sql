82e7c7037f0087618c2c08d84312082c1099429c
Caroline-Quigg <carolinequigg@yahoo.com>
2018-05-16 22:18:12 -0700

    initial commit 2A'

--- hw2/caltrans.sql
+++ hw2/caltrans.sql
@@ -2,12 +2,12 @@ USE hw2;
 
 SELECT highway, area FROM caltrans
 	WHERE text LIKE "IS CLOSE%FOR THE WINTER%" OR text LIKE "IS CLOSED%DUE TO SNOW%"
-	GROUP BY highway, area
-	ORDER BY highway, area DESC
+	GROUP BY highway,area
+	ORDER BY highway DESC, area DESC
 	LIMIT 20;
 
 
-SELECT highway, area, (COUNT(reported)/365 * 100) as percentage FROM caltrans
+SELECT highway, area, (COUNT(DISTINCT reported)/365 * 100) as percentage FROM caltrans
 	WHERE text LIKE "IS CLOSE%FOR THE WINTER%" OR text LIKE "IS CLOSED%DUE TO SNOW%"
 	GROUP BY highway, area
 	ORDER BY percentage DESC
@@ -24,4 +24,42 @@ FROM
 	LEFT JOIN trip_ends ON trip_starts.trip_id = trip_ends.trip_id
 )temp
 LIMIT 5;
+
+-- calculate trip costs per user
+SELECT user_id, 
+	IF(1 + .15 *CEILING(TIME_TO_SEC(trip_length)/60) > 100, 100, 1 + .15 *CEILING(TIME_TO_SEC(trip_length)/60) ) as trip_charge
+	FROM
+	(
+		SELECT trip_starts.trip_id as trip_id,
+		trip_starts.user_id as user_id,
+		TIMEDIFF( 
+		IFNULL(trip_ends.time, TIMESTAMPADD(HOUR, 24, trip_starts.time)), trip_starts.time  ) as trip_length
+		FROM trip_starts
+		LEFT JOIN trip_ends ON trip_starts.trip_id = trip_ends.trip_id
+	)temp
+LIMIT 5
+;
+
+
+-- calculate monthly trips cost 
+SELECT user_id, SUM(trip_charge) as monthly_charge
+FROM (
+	SELECT user_id, 
+	IF(1 + .15 *CEILING(TIME_TO_SEC(trip_length)/60) > 100, 100, 1 + .15 *CEILING(TIME_TO_SEC(trip_length)/60) ) as trip_charge
+	FROM
+	(
+		SELECT trip_starts.trip_id as trip_id,
+		trip_starts.user_id as user_id,
+		TIMEDIFF( 
+		IFNULL(trip_ends.time, TIMESTAMPADD(HOUR, 24, trip_starts.time)), trip_starts.time  ) as trip_length
+		FROM trip_starts
+		LEFT JOIN trip_ends ON trip_starts.trip_id = trip_ends.trip_id
+		WHERE MONTH(trip_starts.time) =3
+	)temp
+)temp2
+GROUP BY user_id
+LIMIT 5;
+
+
+
 
